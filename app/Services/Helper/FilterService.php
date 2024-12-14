90da347ca1f7346abb9e4ec55f61ca7864c901aa
ahmed-albakor <ahmed.albakor206@gmail.com>
2024-12-14 13:36:19 +0300

    Upgrate Filter Service

--- app/Services/Helper/FilterService.php
+++ app/Services/Helper/FilterService.php
@@ -28,7 +28,6 @@ class FilterService
             ? ($filters['sortField'] ?? $defaultSortField)
             : $defaultSortField;
 
-
         return FilterService::applySorting($query, $sortField, $sortOrder)->paginate($filters['limit'] ?? 20);
     }
 
@@ -37,7 +36,16 @@ class FilterService
         if ($search) {
             $query->where(function ($q) use ($search, $fields) {
                 foreach ($fields as $field) {
-                    $q->orWhere($field, 'like', "%$search%");
+                    if (strpos($field, '.') !== false) {
+                        // Handle relationship fields
+                        [$relation, $relationField] = explode('.', $field, 2);
+                        $q->orWhereHas($relation, function ($query) use ($relationField, $search) {
+                            $query->where($relationField, 'like', "%$search%");
+                        });
+                    } else {
+                        // Handle direct fields
+                        $q->orWhere($field, 'like', "%$search%");
+                    }
                 }
             });
         }
@@ -47,11 +55,25 @@ class FilterService
     protected static function applyNumericFilters(Builder $query, array $filters, array $numericFields = [])
     {
         foreach ($numericFields as $field) {
-            if (isset($filters["{$field}_min"])) {
-                $query->where($field, '>=', $filters["{$field}_min"]);
-            }
-            if (isset($filters["{$field}_max"])) {
-                $query->where($field, '<=', $filters["{$field}_max"]);
+            if (strpos($field, '.') !== false) {
+                [$relation, $relationField] = explode('.', $field, 2);
+                if (isset($filters["{$field}_min"])) {
+                    $query->whereHas($relation, function ($q) use ($relationField, $filters, $field) {
+                        $q->where($relationField, '>=', $filters["{$field}_min"]);
+                    });
+                }
+                if (isset($filters["{$field}_max"])) {
+                    $query->whereHas($relation, function ($q) use ($relationField, $filters, $field) {
+                        $q->where($relationField, '<=', $filters["{$field}_max"]);
+                    });
+                }
+            } else {
+                if (isset($filters["{$field}_min"])) {
+                    $query->where($field, '>=', $filters["{$field}_min"]);
+                }
+                if (isset($filters["{$field}_max"])) {
+                    $query->where($field, '<=', $filters["{$field}_max"]);
+                }
             }
         }
         return $query;
@@ -60,11 +82,25 @@ class FilterService
     protected static function applyDateFilters(Builder $query, array $filters, array $dateFields = [])
     {
         foreach ($dateFields as $field) {
-            if (isset($filters["{$field}_from"])) {
-                $query->whereDate($field, '>=', $filters["{$field}_from"]);
-            }
-            if (isset($filters["{$field}_to"])) {
-                $query->whereDate($field, '<=', $filters["{$field}_to"]);
+            if (strpos($field, '.') !== false) {
+                [$relation, $relationField] = explode('.', $field, 2);
+                if (isset($filters["{$field}_from"])) {
+                    $query->whereHas($relation, function ($q) use ($relationField, $filters, $field) {
+                        $q->whereDate($relationField, '>=', $filters["{$field}_from"]);
+                    });
+                }
+                if (isset($filters["{$field}_to"])) {
+                    $query->whereHas($relation, function ($q) use ($relationField, $filters, $field) {
+                        $q->whereDate($relationField, '<=', $filters["{$field}_to"]);
+                    });
+                }
+            } else {
+                if (isset($filters["{$field}_from"])) {
+                    $query->whereDate($field, '>=', $filters["{$field}_from"]);
+                }
+                if (isset($filters["{$field}_to"])) {
+                    $query->whereDate($field, '<=', $filters["{$field}_to"]);
+                }
             }
         }
         return $query;
@@ -73,8 +109,17 @@ class FilterService
     protected static function applyExactMatchFilters(Builder $query, array $filters, array $exactMatchFields = [])
     {
         foreach ($exactMatchFields as $field) {
-            if (isset($filters[$field])) {
-                $query->where($field, $filters[$field]);
+            if (strpos($field, '.') !== false) {
+                [$relation, $relationField] = explode('.', $field, 2);
+                if (isset($filters[$field])) {
+                    $query->whereHas($relation, function ($q) use ($relationField, $filters, $field) {
+                        $q->where($relationField, $filters[$field]);
+                    });
+                }
+            } else {
+                if (isset($filters[$field])) {
+                    $query->where($field, $filters[$field]);
+                }
             }
         }
         return $query;
@@ -83,8 +128,17 @@ class FilterService
     protected static function applyInFilters(Builder $query, array $filters, array $inFields = [])
     {
         foreach ($inFields as $field) {
-            if (isset($filters[$field]) && is_array($filters[$field])) {
-                $query->whereIn($field, $filters[$field]);
+            if (strpos($field, '.') !== false) {
+                [$relation, $relationField] = explode('.', $field, 2);
+                if (isset($filters[$field]) && is_array($filters[$field])) {
+                    $query->whereHas($relation, function ($q) use ($relationField, $filters, $field) {
+                        $q->whereIn($relationField, $filters[$field]);
+                    });
+                }
+            } else {
+                if (isset($filters[$field]) && is_array($filters[$field])) {
+                    $query->whereIn($field, $filters[$field]);
+                }
             }
         }
         return $query;
@@ -93,6 +147,14 @@ class FilterService
     protected static function applySorting(Builder $query, $sortField, $sortOrder)
     {
         $sortOrder = in_array($sortOrder, ['asc', 'desc']) ? $sortOrder : 'desc';
-        return $query->orderBy($sortField, $sortOrder);
+        if (strpos($sortField, '.') !== false) {
+            [$relation, $relationField] = explode('.', $sortField, 2);
+            $query->with([$relation => function ($q) use ($relationField, $sortOrder) {
+                $q->orderBy($relationField, $sortOrder);
+            }]);
+        } else {
+            $query->orderBy($sortField, $sortOrder);
+        }
+        return $query;
     }
 }
