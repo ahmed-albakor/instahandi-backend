68b2595a6b76913b93c954017f676d33584ddc52
ahmed-albakor <ahmed.albakor206@gmail.com>
2024-12-19 12:12:26 +0300

    get clinet review 3

--- app/Services/System/VendorService.php
+++ app/Services/System/VendorService.php
@@ -40,7 +40,7 @@ class VendorService
         $user_id = Auth::id();
         $user = User::find($user_id);
 
-        $user->load(['vendor.services', 'vendor.reviews.client', 'images', 'location']);
+        $user->load(['vendor.services', 'vendor.reviews.client.user', 'images', 'location']);
 
         return $user;
     }
