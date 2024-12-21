f87f5a0c035fb5fb80e0dd8c883adf40103c79ea
ahmed-albakor <ahmed.albakor206@gmail.com>
2024-12-21 23:42:06 +0300

    no proposals for vendor 3

--- app/Http/Resources/ProposalResource.php
+++ app/Http/Resources/ProposalResource.php
@@ -15,21 +15,21 @@ class ProposalResource extends JsonResource
             $hidden = $user->role == 'vendor' && $this->vendor_id != $user->vendor->id;
         }
 
-        if (!$hidden)
-            return [
-                'id' => $this->id,
-                'code' => $this->code,
-                'service_request_id' => $this->service_request_id,
-                'vendor_id' => $this->vendor_id,
-                'message' => $this->message,
-                'status' => $this->status,
-                'price' => $this->price,
-                'payment_type' => $this->payment_type,
-                'estimated_hours' => $this->estimated_hours,
-                'created_at' => $this->created_at,
-                'updated_at' => $this->updated_at,
-                'service_request' => new ServiceRequestResource($this->whenLoaded('serviceRequest')),
-                'vendor' => new VendorResource($this->whenLoaded('vendor')),
-            ];
+
+        return $this->when(! $hidden, [
+            'id' => $this->id,
+            'code' => $this->code,
+            'service_request_id' => $this->service_request_id,
+            'vendor_id' => $this->vendor_id,
+            'message' => $this->message,
+            'status' => $this->status,
+            'price' => $this->price,
+            'payment_type' => $this->payment_type,
+            'estimated_hours' => $this->estimated_hours,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'service_request' => new ServiceRequestResource($this->whenLoaded('serviceRequest')),
+            'vendor' => new VendorResource($this->whenLoaded('vendor')),
+        ]);
     }
 }
