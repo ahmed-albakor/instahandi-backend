477c2faeea7306a626be437ca3eba5906669ab10
ahmed-albakor <ahmed.albakor206@gmail.com>
2024-12-25 18:28:00 +0300

    Refactor email validation in client update request to use Rule::unique with ignore for improved clarity and maintainability

--- app/Http/Requests/Client/UpdateRequest.php
+++ app/Http/Requests/Client/UpdateRequest.php
@@ -4,6 +4,8 @@ namespace App\Http\Requests\Client;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
 
 class UpdateRequest extends FormRequest
 {
@@ -19,7 +21,13 @@ class UpdateRequest extends FormRequest
         return [
             'user.first_name' => 'nullable|string|max:255',
             'user.last_name' => 'nullable|string|max:255',
-            'user.email' => 'nullable|string|email|max:100|' . (Auth::user()->email == request()->input('user.email') ? '' : 'unique:users,email'),
+            'user.email' => [
+                'nullable',
+                'string',
+                'email',
+                'max:100',
+                Rule::unique('users', 'email')->ignore(Auth::id()),
+            ],
             'user.phone' => 'nullable|string|max:55',
             'user.gender' => 'nullable|in:male,female',
             'additional_images' => 'nullable|array|max:3',
