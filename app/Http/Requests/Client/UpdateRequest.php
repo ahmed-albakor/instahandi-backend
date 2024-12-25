b51c531c534c82075aef4ab19a5da26ff74e86bf
ahmed-albakor <ahmed.albakor206@gmail.com>
2024-12-25 18:23:44 +0300

    Refactor email uniqueness validation in client update request to conditionally apply unique rule for improved clarity

--- app/Http/Requests/Client/UpdateRequest.php
+++ app/Http/Requests/Client/UpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Client;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UpdateRequest extends FormRequest
 {
@@ -18,7 +19,7 @@ class UpdateRequest extends FormRequest
         return [
             'user.first_name' => 'nullable|string|max:255',
             'user.last_name' => 'nullable|string|max:255',
-            'user.email' => 'nullable|string|email|max:100|unique:users,email,' . $this->client->user->id,
+            'user.email' => 'nullable|string|email|max:100|' . (Auth::user()->email == request()->input('user.email') ? '' : 'unique:users,email'),
             'user.phone' => 'nullable|string|max:55',
             'user.gender' => 'nullable|in:male,female',
             'additional_images' => 'nullable|array|max:3',
