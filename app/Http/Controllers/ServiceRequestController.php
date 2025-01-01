d97083f3325f061ac1b33d93b94e6ffd194f32f6
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-01-01 20:17:24 +0300

    Refactor payment handling in hireVendor method to improve clarity and consistency

--- app/Http/Controllers/ServiceRequestController.php
+++ app/Http/Controllers/ServiceRequestController.php
@@ -13,6 +13,7 @@ use App\Models\ServiceRequest;
 use App\Permissions\ServiceRequestPermission;
 use App\Services\Helper\ResponseService;
 use App\Services\Helper\StripeService;
+use App\Services\System\ClientPaymentService;
 use App\Services\System\OrderService;
 use App\Services\System\ProposalService;
 use App\Services\System\ServiceRequestService;
@@ -217,20 +218,22 @@ class ServiceRequestController extends Controller
         $payments = $serviceRequest->payments()->where('status', 'pending')->get();
 
         foreach ($payments as $payment) {
-            $payment_data = json_decode(json: json_encode($payment->payment_data));
+            $paymentData = json_decode(json_encode($payment->payment_data));
 
             $stripeService = new StripeService();
+            $clientPaymentService = new ClientPaymentService();
+            $payment = $clientPaymentService->getPaymentById($payment->id);
 
-            $paymentIntent = $stripeService->retrievePaymentIntent($payment_data->id);
+            $paymentIntent = $stripeService->retrievePaymentIntent($paymentData->id);
 
             if ($paymentIntent->status === 'succeeded') {
-                $payment->update($payment, [
-                    'status' => 'confirm',
-                    'payment_data' => json_encode($paymentIntent),
-                ]);
+            $payment->updatePayment($payment, [
+                'status' => 'confirm',
+                'payment_data' => json_encode($paymentIntent),
+            ]);
 
-                $serviceRequest->can_job = 1;
-                $serviceRequest->save();
+            $serviceRequest->can_job = 1;
+            $serviceRequest->save();
             }
         }
 
